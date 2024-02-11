016f38808171ca3e54fa2b3fc89e491f6a7bd8c7
Egor Arbuzov <egorarbuzov@gmail.com>
2024-02-11 21:18:53 +0300

    Delete devise form gem file && recreate model

--- db/migrate/20240208085626_change_default_values_tables.rb
+++
@@ -1,6 +0,0 @@
-class ChangeDefaultValuesTables < ActiveRecord::Migration[6.1]
-  def change
-    change_column :tests, :level, :numeric, default: 1
-    change_column :answers, :correct, :boolean, default: false
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2024_02_08_085626) do
+ActiveRecord::Schema.define(version: 2024_02_09_092023) do
 
   create_table "answers", force: :cascade do |t|
-    t.text "body", null: false
-    t.boolean "correct", default: false, null: false
+    t.string "body", null: false
+    t.boolean "correct", default: false
     t.integer "question_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -22,13 +22,13 @@ ActiveRecord::Schema.define(version: 2024_02_08_085626) do
   end
 
   create_table "categories", force: :cascade do |t|
-    t.string "title", null: false
+    t.string "title"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "questions", force: :cascade do |t|
-    t.text "body", null: false
+    t.string "body"
     t.integer "test_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -36,8 +36,9 @@ ActiveRecord::Schema.define(version: 2024_02_08_085626) do
   end
 
   create_table "tests", force: :cascade do |t|
-    t.string "title", null: false
-    t.decimal "level", default: "1.0", null: false
+    t.string "title"
+    t.decimal "level", default: "1.0"
+    t.decimal "numeric", default: "1.0"
     t.integer "category_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
@@ -45,15 +46,9 @@ ActiveRecord::Schema.define(version: 2024_02_08_085626) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string "email", default: "", null: false
-    t.string "encrypted_password", default: "", null: false
-    t.string "reset_password_token"
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
+    t.string "username", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["email"], name: "index_users_on_email", unique: true
-    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "answers", "questions"
