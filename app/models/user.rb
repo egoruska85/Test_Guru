1ed4d4027e2eed0d2810b0addd5fd0dd176a59ad
Egor Arbuzov <egorarbuzov@gmail.com>
2024-02-22 01:33:23 +0300

    correcting

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
   has_many :tests_users
   has_many :tests, through: :tests_users
 
-  def finding(level)
+  def finding(level:)
     tests.where(level: level)
   end
 end
