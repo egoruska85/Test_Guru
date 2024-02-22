d483696c0f33595237a5497b4661cd3d671df252
Egor Arbuzov <egorarbuzov@gmail.com>
2024-02-22 15:14:08 +0300

    fix error 2

--- app/models/test.rb
+++ app/models/test.rb
@@ -4,6 +4,6 @@ class Test < ApplicationRecord
   has_many :users, through: :tests_users
 
   def self.find_test_by_category(name)
-    Test.joins(:category).where(category: {title: name})
+    joins(:category).where(category: {title: name})
   end
 end
