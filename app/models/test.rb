43cc2ab0b5823195948845df234243558f7ba2da
Egor Arbuzov <egorarbuzov@gmail.com>
2024-03-24 15:54:42 +0300

    Задание к уроку "Ассоциации моделей"

--- app/models/test.rb
+++ app/models/test.rb
@@ -1,7 +1,9 @@
 class Test < ApplicationRecord
   belongs_to :category
+  has_many :questions
   has_many :tests_users
   has_many :users, through: :tests_users
+  has_one :user
 
   def self.find_test_by_category(name)
     joins(:category).where(category: {title: name}).order(id: :desc)
